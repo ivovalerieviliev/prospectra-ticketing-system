7277a05591caf476ddbd680d532bd183dc270a12
ivovalerieviliev <ivaylo.i@prospectra.org>
2025-12-03 15:21:47 +0200

    Implement PTS_Loader class for plugin management
    Added PTS_Loader class to handle plugin initialization and hook registration.

--- includes/core/class-pts-loader.php
+++ includes/core/class-pts-loader.php
@@ -1,2 +1,73 @@
-<?php
-// class-pts-loader.php placeholder.
+<? php
+/**
+ * Plugin Loader - Handles initialization sequence
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class PTS_Loader {
+    
+    /**
+     * Actions to register
+     */
+    protected $actions = array();
+    
+    /**
+     * Filters to register
+     */
+    protected $filters = array();
+    
+    /**
+     * Add action
+     */
+    public function add_action($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
+        $this->actions = $this->add($this->actions, $hook, $component, $callback, $priority, $accepted_args);
+    }
+    
+    /**
+     * Add filter
+     */
+    public function add_filter($hook, $component, $callback, $priority = 10, $accepted_args = 1) {
+        $this->filters = $this->add($this->filters, $hook, $component, $callback, $priority, $accepted_args);
+    }
+    
+    /**
+     * Add hook to collection
+     */
+    private function add($hooks, $hook, $component, $callback, $priority, $accepted_args) {
+        $hooks[] = array(
+            'hook' => $hook,
+            'component' => $component,
+            'callback' => $callback,
+            'priority' => $priority,
+            'accepted_args' => $accepted_args,
+        );
+        
+        return $hooks;
+    }
+    
+    /**
+     * Register hooks with WordPress
+     */
+    public function run() {
+        foreach ($this->filters as $hook) {
+            add_filter(
+                $hook['hook'],
+                array($hook['component'], $hook['callback']),
+                $hook['priority'],
+                $hook['accepted_args']
+            );
+        }
+        
+        foreach ($this->actions as $hook) {
+            add_action(
+                $hook['hook'],
+                array($hook['component'], $hook['callback']),
+                $hook['priority'],
+                $hook['accepted_args']
+            );
+        }
+    }
+}
